0c765ebd45f3ff0a3c38d32ed17d00b6cea5dc1d
William Hood <billylhood@gmail.com>
2022-09-24 14:20:19 -0400

    initial commit
    Melpa and Gelpa setup, Inkpot theme, menu tool scroll bar off, line numbers on

---
+++ init.el
@@ -0,0 +1,43 @@
+;; Line Mode
+(global-display-line-numbers-mode 1)
+;;Menu bars
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; Define and initialise package repositories
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;; use-package to simplify the config file
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure 't)
+
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+
+
+;; Theme
+(use-package inkpot-theme
+  :config (load-theme 'inkpot t))
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
